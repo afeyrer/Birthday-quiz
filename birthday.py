67b293827bdb4c4a19190e9dfd5b8fd250abdbb5
afeyrer <abigailfeyrer@hanovernorwichschools.org>
2017-02-07 14:38:12 -0500

    Updated from Brython Server: 2/7/2017 2:38:12 PM

--- birthday.py
+++ birthday.py
@@ -31,9 +31,35 @@ Example Session
   And the day? 11
   Eric, you are a fall baby of the stone age.
 """
+from datetime import datetime
+from calendar import month_name
+todaymonth = datetime.today().month
+todaydate = datetime.today().day
+monthrn = month_name[todaymonth]
 
 name=input("Hello, what is your name? ")
 month=input("Hi "+name+", what was the name of the month you were born in? ")
 year=input("And what year were you born in, "+name+"? ")
 day=input("And the day? ")
 
+if int(day)==31 and month==("October"):
+    print("You were born on Halloween! ")
+if int(day)==todaydate and month==monthrn:
+    print("Happy birthday! ")
+    
+if month==("December") or month==("January") or month==("February"):
+    season="winter"
+if month==("March") or month==("April") or month==("May"):
+    season="spring"
+ if month==("June") or month==("July") or month==("August"):
+    season="Summer"  
+if month==("September") or month==("October") or month==("November"):
+    season="fall"
+if int(year)<=1989 and int(year)>=1980:
+    timeperiod="eighties"
+if int(year)<1980:
+    timeperiod="Stone Age"
+if int(year)<=1999 and int(year)>=1990:
+    timeperiod="nineties"
+if int(year)>=2000:
+    timeperiod="two thousands"
